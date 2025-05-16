a28addd2a022c8cd7c03e123bccb029362a4120b
Jan Trummer <jan.trummer@hotmail.com>
2025-05-16 22:26:20 +0200

    Capture mouse when UI is open

--- AssaultCubeCheat/UI.cpp
+++ AssaultCubeCheat/UI.cpp
@@ -1,6 +1,7 @@
 #include "UI.h"
 #include <Windows.h>
 #include <iostream>
+#include <SDL.h>
 
 #include "imgui/imgui.h"
 
@@ -13,6 +14,24 @@ namespace UI
 		if (GetAsyncKeyState(VK_INSERT) & (0x8000 != 0))
 		{
 			shouldDrawUI = !shouldDrawUI;
+
+			if (shouldDrawUI)
+			{
+				ShowCursor(true);
+				SDL_CaptureMouse(SDL_FALSE);
+				SDL_SetRelativeMouseMode(SDL_FALSE);
+				SDL_SetHint(SDL_HINT_MOUSE_RELATIVE_MODE_CENTER, "0");
+				SetCursor(LoadCursor(NULL, IDC_ARROW));
+				SDL_ShowCursor(true);
+			}
+			else
+			{
+				SDL_ShowCursor(false);
+				SDL_CaptureMouse(SDL_TRUE);
+				SDL_SetRelativeMouseMode(SDL_TRUE);
+				SDL_SetHint(SDL_HINT_MOUSE_RELATIVE_MODE_CENTER, "1");
+				ShowCursor(false);
+			}
 		}
 
 		if (!shouldDrawUI) return;
